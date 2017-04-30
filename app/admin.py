ad0cb1273251b1e1a91ed43fcfc6a1a299833322
maykham <may.yaaron@gmail.com>
2017-05-01 00:57:24 +0300

    YogaApp first commit

--- app/admin.py
+++ app/admin.py
@@ -16,25 +16,6 @@ class WaitingInline(admin.TabularInline):
 @admin.register(models.Lesson)
 class LessonAdmin(admin.ModelAdmin):
     inlines = [RegistrationInline, WaitingInline]
-    exclude = ['code']
-    list_display = ['day','time','num_enrolled','regular']
+    exclude = 'code'
+    list_display = ('day', 'time', 'num_enrolled', 'regular')
 
-#  ⁠⁠⁠class InqAdmin(admin.ModelAdmin):
-# list_display = ('name','company', 'email', 'subject','date')
-#
-# class JobAdmin(admin.ModelAdmin):
-# list_display = ('headline','date_added', 'active')
-#
-# class NewsAdmin(admin.ModelAdmin):
-# list_display = ('headline','date_added', 'active')
-#
-#
-# admin.site.register(OnePager)
-# admin.site.register(Platform)
-# admin.site.register(Solution)
-# admin.site.register(Solutions)
-# admin.site.register(TeamMember)
-# admin.site.register(About)
-# admin.site.register(News,NewsAdmin)
-# admin.site.register(JobPosition, JobAdmin)
-# admin.site.register(Inquiry, InqAdmin)
