0cab101b307c47343061b07e8ef1755658ad02d6
maykham <may.yaaron@gmail.com>
2017-05-01 09:21:08 +0300

    YogaApp first commit

--- app/admin.py
+++ app/admin.py
@@ -8,14 +8,14 @@ admin.site.register(models.Person)
 
 
 class RegistrationInline(admin.TabularInline):
-    fieldsets = (
-        (
-            None,
-            {
-                'fields': ('person',)
-            }
-        ),
-    )
+    # fieldsets = (
+    #     (
+    #         None,
+    #         {
+    #             'fields': ('person',)
+    #         }
+    #     ),
+    # )
     model = models.Registration
 
 class WaitingInline(admin.TabularInline):
