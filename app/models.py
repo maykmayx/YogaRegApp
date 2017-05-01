e29cd532f1ee7da84d3cbeccabf4cc77393da5e6
maykham <may.yaaron@gmail.com>
2017-05-01 18:05:51 +0300

    YogaApp first commit

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import date, datetime
 
 from django.db import models
 from django.db.models.signals import pre_delete
-from django.dispatch import receiver
+from django.dispatch import Signal
 
 
 class Person(models.Model):
@@ -33,10 +33,13 @@ class Lesson(models.Model):
         date_formatted = self.day.strftime('%d') + '/' + self.day.strftime('%m')
         return date_formatted + ' @%s' % (self.time)
 
+def rem_reg(self):
+    self.num_enrolled -= 1
+    return
 
 class Registration(models.Model):
     person = models.ForeignKey(Person)
-    lesson = models.ForeignKey(Lesson)
+    lesson = models.ForeignKey(Lesson, on_delete=rem_reg)
     #
     # def delete(self, **kwargs):
     #     self.lesson.decrease_num()
@@ -45,15 +48,23 @@ class Registration(models.Model):
     #     return
 
 
+
+
 class Waiting(models.Model):
     person = models.ForeignKey(Person)
     lesson = models.ForeignKey(Lesson)
 
+# rem_reg = Signal(providing_args=["lesson"])
+#
+# def send_rem_reg(self, lesson):
+#
 
-@receiver(pre_delete, sender=Registration)
-def remove_reg(sender, instance, **kwargs):
-    reg = Registration.objects.get(pk=instance.pk)
-    reg.lesson.decrease_num()
+
+#
+# @receiver(pre_delete, sender=Registration)
+# def remove_reg(sender, instance, **kwargs):
+#     reg = Registration.objects.get(pk=instance.pk)
+#     reg.lesson.decrease_num()
 
 # pre_delete.connect(remove_reg, sender=Registration)
 
