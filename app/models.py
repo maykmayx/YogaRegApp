7abd1a73e85694a338dd6a66585ede82dc4f3f61
maykham <may.yaaron@gmail.com>
2017-05-01 14:32:29 +0300

    YogaApp first commit

--- app/models.py
+++ app/models.py
@@ -19,8 +19,8 @@ class Lesson(models.Model):
     num_enrolled = models.IntegerField(default=0, editable=False)
     regular = models.BooleanField(default=True)
 
-    class Meta:
-        ordering = ('day',)
+    # class Meta:
+        # ordering = ('day'
     # code = models.CharField(max_length=1024, editable=False)
 
     # (mod 7) + 1 to permute to hebrew schedule
@@ -30,7 +30,7 @@ class Lesson(models.Model):
 
     def __unicode__(self):
         date_formatted = self.day.strftime('%d') + '/' + self.day.strftime('%m')
-        return date_formatted + ' %s בשעה' % (self.time)
+        return date_formatted + ' @%s' % (self.time)
 
 
 class Registration(models.Model):
