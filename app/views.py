7a3111a0ae88be15c21fe90442f18fd074c20394
maykham <may.yaaron@gmail.com>
2017-05-07 14:27:01 +0300

    YogaApp first commit

--- app/views.py
+++ app/views.py
@@ -101,7 +101,8 @@ def submit_lessons(request):
     date_object = datetime.datetime.strptime(input_date, '%Y-%m-%d')
     lessons = models.Lesson.objects.filter(regular=True)
     for lesson in lessons:
-        delta = (lesson.day.strftime('%u') % 7) + 1
+        day_code = lesson.day.strftime('%u')
+        delta = (day_code % 7) + 1
         day = date_object + datetime.timedelta(days=delta)
         time = lesson.time
         max_participants = lesson.max_participants
