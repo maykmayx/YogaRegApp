b0ae5648d84385a921a42f4c30171e5918d9a938
maykham <may.yaaron@gmail.com>
2017-05-09 23:57:23 +0300

    final fix for page of lessons select - add "full" option

--- app/admin.py
+++ app/admin.py
@@ -14,15 +14,6 @@ admin.site.register(models.Person)
 class RegistrationInline(admin.TabularInline):
     model = models.Registration
     list_display = ('person',)
-    #
-    # def get_name(self, obj):
-    #     return obj.person.name
-    #
-    # get_name.admin_order_field = 'person'  # Allows column order sorting
-    # get_name.short_description = 'Person name'  # Renames column head
-
-    # actions = [deleteReg]
-    # list_display = ('person__name',)
 
 
 class WaitingInline(admin.TabularInline):
@@ -33,38 +24,10 @@ class WaitingInline(admin.TabularInline):
 class LessonAdmin(admin.ModelAdmin):
     inlines = [RegistrationInline, WaitingInline]
     exclude = ('code',)
-    list_display = ('day', 'time', 'num_enrolled', 'regular')
+    list_display = ('day', 'time', 'num_enrolled', 'regular', 'full')
     date_hierarchy = 'day'
-    list_filter = ('day',)
+    list_filter = (('day',DateFieldListFilter),)
 
-    # def save_formset(self, request, form, formset, change):
-    #     super(LessonAdmin).save_formset(request, form, formset, change)
-    #     if formset.model == models.Registration:
-    #         obj = formset.instance
-    #         if obj.reformat:
-    #             form.instance.num_enrolled -= 1
-    #             obj.delete()
-    #         obj.save()
-    # def save_formset(self, request, form, formset, change):
-    #     # super(LessonAdmin, self).save_formset(request, form, formset, change)
-    #     instances = formset.save()
-    #     for instance in instances:
-    #         if instance.model == models.Lesson:
-    #             instance.num_enrolled -= 1
 
-    #     # #     obj.save()
-    #     # for obj in formset.deleted_objects:
-    #     #     obj.my_delete()
-    #     #     # obj.save()
-    #     # formset.save()
-    # #         obj.lesson.num_enrolled -= 1
-    # #     obj.save()
-    #                     # creating new objects
-    #     # instances = formset.save(commit=False)
-    #     # for instance in instances:
-    #     #     instance.save()
-    #     # for obj in formset.deleted_objects:
-    #     #     obj.lesson.num_enrolled -= 1
-    #     #     obj.delete()
 
 
