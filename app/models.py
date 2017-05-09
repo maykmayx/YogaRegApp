8c7ed437454d5a8f482b7ea12081f8eeb0dee7fd
maykham <may.yaaron@gmail.com>
2017-05-09 22:31:37 +0300

    signal fix - call specific lesson's function, not iterate on all

--- app/models.py
+++ app/models.py
@@ -72,3 +72,4 @@ class Lesson(models.Model):
 # Rerun the count for lesson's num enrolled whenever a Change is saved or deleted.
 signals.post_save.connect(update_count, sender=Registration)
 signals.post_delete.connect(update_count, sender=Registration)
+
--- app/signals.py
+++ app/signals.py
@@ -1,12 +1,12 @@
 from django.db.models import signals
 from django.dispatch import dispatcher
 
-
 def update_count(sender, instance, signal, *args, **kwargs):
   """
   Runs through all the lessons and updates their num enrolled field
   """
-  from app.models import Lesson
-  for lesson in Lesson.objects.all():
-    lesson.update_num_enrolled()
 
+  try:
+      instance.lesson.update_num_enrolled()
+  except:
+      print("error: can't update lesson num_enrolled")
