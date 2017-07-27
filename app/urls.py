e081572e3fd5a906e56ee50b0b863b5ea3f005d9
maykham <may.yaaron@gmail.com>
2017-07-27 12:53:13 +0300

    lesson model 'full' field fix

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ app_name = 'app'
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='summer_index'),
     url(r'^(?P<week_date>\d+/\d+-\d+/\d+)', views.week, name='week'),
     url(r'^register$', views.register, name='register'),
     url(r'^create_lessons$', views.create_lessons, name='create_lessons'),
